869d290c72131d04883cf4c2ae42fb82f72b291d
RomanCech <81cechroman@gmail.com>
2024-04-14 19:48:14 +0200

    React 18 useEffect runs twice fixed

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -2,7 +2,7 @@ import {useNavigate} from "react-router-dom";
 import apiService from "../api/ApiService";
 import authAtom from "../api/authAtom";
 import {useRecoilState} from "recoil";
-import React, {useEffect} from "react";
+import React, {useEffect, useRef} from "react";
 import axios from "axios";
 import {IAuthResponse} from "../common/Types";
 import Spinner from "./Spinner";
@@ -10,18 +10,23 @@ import Spinner from "./Spinner";
 const Logout: React.FC = () => {
     const [auth, setAuth] = useRecoilState<IAuthResponse | null>(authAtom)
     const navigate = useNavigate()
+    const isMounted = useRef(false)
 
     useEffect(() => {
-        if (auth?.token?.accessToken !== undefined) {
-            apiService.logout().then(_ => {
-                setAuth(null)
-                localStorage.removeItem('user')
-                axios.defaults.headers.common.Authorization = ''
-                navigate('/')
-            }).catch(e => {
-                // Handle login error, TODO
-                console.log('Error: ', e)
-            })
+        if(!isMounted.current) {
+            isMounted.current = true
+
+            if (auth?.token?.accessToken !== undefined) {
+                apiService.logout().then(_ => {
+                    setAuth(null)
+                    localStorage.removeItem('user')
+                    axios.defaults.headers.common.Authorization = ''
+                    navigate('/')
+                }).catch(e => {
+                    // Handle login error
+                    console.log('Error: ', e)
+                })
+            }
         }
     }, [])
 
